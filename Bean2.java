88671e578d73afc3f4290d79afa81a73b7cc5771
Vladir1 <76425332+Vladir1@users.noreply.github.com>
2024-02-26 11:15:46 +0200

    Create Bean2.java

---
+++ Bean2.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Bean2 {
+    private String value;
+
+    // Getter для поля value
+    public String getValue() {
+        return value;
+    }
+
+    // Setter для поля value
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
